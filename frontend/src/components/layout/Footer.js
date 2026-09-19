7c694f777b8696d1ed229639b290398e560c11fa
Namogo YEO <yeo.daouda@pnls-ci.com>
2026-09-19 12:08:36 +0000

    Modification apportée au Footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { FormattedMessage, useIntl } from "react-intl";
 
 function Footer() {
+  const intl = useIntl();
   return (
     <footer className="footer py-2 mt-auto">
-      <p className="m-0 text-center">
-        {/* © OpenElis Global2 2020. All rights reserved. */}
+      <p className="m-0 text-center"><br />
+       {/* © OpenElis Global2 2020. All rights reserved. */}<br /><br /><br /><br /><br /><br /><br /><br /><br />
+        {intl.formatMessage({ id: "atelier.g2.footer.ministere" })}<br />
+        {intl.formatMessage({ id: "atelier.g2.footer.direction" })}
       </p>
     </footer>
   );
